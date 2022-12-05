083f44004fca33efb8734aef2a506e8979717275
rikiya miyazawa <rky.weed.rss@gmail.com>
2022-12-05 15:40:18 +0900

    feat:文章、画像登録用のモデル、CRUD機能用のファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'pictures/index'
+  get 'pictures/new'
+  get 'pictures/show'
+  get 'pictures/edit'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
