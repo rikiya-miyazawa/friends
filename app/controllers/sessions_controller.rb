fa61102b76e6fe4ef5987ddc8155791dd42ed93a
rikiya miyazawa <rky.weed.rss@gmail.com>
2022-12-05 15:16:13 +0900

    feat:ログアウト機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,9 @@ class SessionsController < ApplicationController
     end
   end
   
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
+  end
 end
