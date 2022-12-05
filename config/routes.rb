19a108ddf286f27b556c05e40f887232b1980e15
rikiya miyazawa <rky.weed.rss@gmail.com>
2022-12-05 16:05:08 +0900

    feat:pictureのルートを記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'pictures/index'
-  get 'pictures/new'
-  get 'pictures/show'
-  get 'pictures/edit'
+  resources :pictures
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
