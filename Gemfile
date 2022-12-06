575f5150b1035da00d17039fdb62e09c1b42e150
rikiya miyazawa <rky.weed.rss@gmail.com>
2022-12-06 12:04:23 +0900

    feat:画像アップロードするため、アップロードボタンと、確認画面実装

--- Gemfile
+++ Gemfile
@@ -33,4 +33,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bcrypt'
 gem 'carrierwave'
+gem 'mimemagic'
 gem 'mini_magick'
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,7 @@
 class Picture < ApplicationRecord
   belongs_to :user
+  mount_uploader :image, ImageUploader
+  validates :image, presence: true
   validates :content, presence: true,
             length: { maximum: 255 }
 end
